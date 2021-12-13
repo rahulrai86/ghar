6c9409588fd7acd9cedbb1248cc8701f1ed16ddf
rahul shrivastava <rahulrai86@gmail.com>
2021-12-13 13:00:48 +0530

    dekh bhai ghar mai maine right click ka program likha hai

--- test1.py
+++ test1.py
@@ -1,9 +1,14 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ActionChains
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("https://www.google.com/")
 driver.maximize_window()
-
-rahul = "hi i am rahul"
-print(rahul)
+time.sleep(5)
+gmail=driver.find_element(By.XPATH,"//a[normalize-space()='Gmail']")
+action = ActionChains(driver)
+time.sleep(5)
+action.context_click(gmail).perform()
